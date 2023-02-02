a55c33d0e26a88fed40d9aa73689b5a473197675
kolima <lord.kolima@gmail.com>
2023-02-02 17:01:50 +0200

    add cicd

--- test/main.ts
+++ test/main.ts
@@ -1,77 +1,65 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
-const ONE_USDC = ethers.BigNumber.from("1000000");
-const ONE_WETH = ethers.BigNumber.from("1000000000000000000");
-const MAX_UINT128 = ethers.BigNumber.from("340282366920938463463374607431768211455");
+const ONE_USDC = ethers.BigNumber.from('1000000')
+const ONE_WETH = ethers.BigNumber.from('1000000000000000000')
+const MAX_UINT128 = ethers.BigNumber.from('340282366920938463463374607431768211455')
 
-describe("RFQ", function () {
-
+describe('RFQ', function () {
   async function setupTest() {
     const [vaultOwner, borrower, tokenDeployer] = await ethers.getSigners()
 
     // deploy vault
-    const Vault = await ethers.getContractFactory("Vault")
+    const Vault = await ethers.getContractFactory('Vault')
     await Vault.connect(vaultOwner)
     const vault = await Vault.deploy()
     await vault.deployed()
 
     // deploy test tokens
-    const MyERC20 = await ethers.getContractFactory("MyERC20")
+    const MyERC20 = await ethers.getContractFactory('MyERC20')
 
     const USDC = await MyERC20.connect(tokenDeployer)
-    const usdc = await USDC.deploy("USDC", "USDC", 6)
+    const usdc = await USDC.deploy('USDC', 'USDC', 6)
     await usdc.deployed()
 
     const WETH = await MyERC20.connect(tokenDeployer)
-    const weth = await WETH.deploy("WETH", "WETH", 18)
+    const weth = await WETH.deploy('WETH', 'WETH', 18)
     await weth.deployed()
 
     // transfer some test tokens
     await usdc.mint(vaultOwner.address, ONE_USDC.mul(100000))
     await weth.mint(borrower.address, ONE_WETH.mul(10))
 
-    return { vault, vaultOwner, borrower, tokenDeployer, usdc, weth };
+    return { vault, vaultOwner, borrower, tokenDeployer, usdc, weth }
   }
 
-  describe("...", function () {
-    it("...", async function () {
-      const { vault, vaultOwner, borrower, tokenDeployer, usdc, weth } = await setupTest();
+  describe('...', function () {
+    it('...', async function () {
+      const { vault, vaultOwner, borrower, tokenDeployer, usdc, weth } = await setupTest()
 
       // vault owner deposits usdc
-      await usdc.connect(vaultOwner).transfer(vault.address, ONE_USDC.mul(100000));
+      await usdc.connect(vaultOwner).transfer(vault.address, ONE_USDC.mul(100000))
 
       // vault owner gives quote
-      const blocknum = await ethers.provider.getBlockNumber();
-      const timestamp = (await ethers.provider.getBlock(blocknum)).timestamp;
+      const blocknum = await ethers.provider.getBlockNumber()
+      const timestamp = (await ethers.provider.getBlock(blocknum)).timestamp
       let loanQuote = {
         borrower: borrower.address,
         collToken: weth.address,
         loanToken: usdc.address,
         sendAmount: ONE_WETH,
         loanAmount: ONE_USDC.mul(1000),
-        expiry: timestamp+60*60*24*30,
+        expiry: timestamp + 60 * 60 * 24 * 30,
         earliestRepay: timestamp,
         repayAmount: ONE_USDC.mul(1010),
-        validUntil: timestamp+60,
+        validUntil: timestamp + 60,
         upfrontFee: ONE_WETH.mul(50).div(10000),
         v: undefined,
         r: undefined,
-        s: undefined,
+        s: undefined
       }
       const payload = ethers.utils.defaultAbiCoder.encode(
-        [
-          "address",
-          "address",
-          "address",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256",
-          "uint256"
-        ],
+        ['address', 'address', 'address', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
         [
           loanQuote.borrower,
           loanQuote.collToken,
@@ -90,8 +78,8 @@ describe("RFQ", function () {
       const signature = await vaultOwner.signMessage(ethers.utils.arrayify(payloadHash))
       const sig = ethers.utils.splitSignature(signature)
       const recoveredAddr = ethers.utils.verifyMessage(ethers.utils.arrayify(payloadHash), sig)
-      console.log("payloadHash:", payloadHash)
-      console.log("Signature:", sig)
+      console.log('payloadHash:', payloadHash)
+      console.log('Signature:', sig)
       expect(recoveredAddr).to.equal(vaultOwner.address)
 
       // borrower adds sig to quote
@@ -109,7 +97,7 @@ describe("RFQ", function () {
       const vaultUsdcBalPre = await usdc.balanceOf(vault.address)
 
       // borrower executes quote
-      const tx = await vault.connect(borrower).borrowWithQuote(loanQuote, "0x0000000000000000000000000000000000000000", "0x")
+      const tx = await vault.connect(borrower).borrowWithQuote(loanQuote, '0x0000000000000000000000000000000000000000', '0x')
 
       // check balance post borrow
       const borrowerWethBalPost = await weth.balanceOf(borrower.address)
@@ -121,8 +109,8 @@ describe("RFQ", function () {
       expect(borrowerUsdcBalPost.sub(borrowerUsdcBalPre)).to.equal(vaultUsdcBalPre.sub(vaultUsdcBalPost))
 
       // borrower cannot replay quote
-      await expect(vault.connect(borrower).borrowWithQuote(loanQuote, "0x0000000000000000000000000000000000000000", "0x")).to.be.reverted
-    });
+      await expect(vault.connect(borrower).borrowWithQuote(loanQuote, '0x0000000000000000000000000000000000000000', '0x')).to
+        .be.reverted
+    })
   })
-
-});
+})
