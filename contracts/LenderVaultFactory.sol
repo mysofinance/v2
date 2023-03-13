5ce84f84bb341737895bae554d01f1df9c32e80a
asardon <as@myso.finance>
2023-03-13 18:04:08 +0100

    remove unused imports etc.

--- contracts/LenderVaultFactory.sol
+++ contracts/LenderVaultFactory.sol
@@ -2,13 +2,11 @@
 
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {ILenderVault} from "./interfaces/ILenderVault.sol";
 import {ILenderVaultFactory} from "./interfaces/ILenderVaultFactory.sol";
 import {IAddressRegistry} from "./interfaces/IAddressRegistry.sol";
-import {DataTypes} from "./DataTypes.sol";
 
 contract LenderVaultFactory is ILenderVaultFactory, ReentrancyGuard {
     address public addressRegistry;
