0dc89cde8796ce32c021750a1b9ad46f08b18e6e
asardon <as@myso.finance>
2023-06-23 17:36:29 +0200

    removed redundant imports

--- contracts/peer-to-peer/oracles/chainlink/ChainlinkBasic.sol
+++ contracts/peer-to-peer/oracles/chainlink/ChainlinkBasic.sol
@@ -3,10 +3,6 @@
 pragma solidity 0.8.19;
 
 import {ChainlinkBase} from "./ChainlinkBase.sol";
-import {AggregatorV3Interface} from "../../interfaces/oracles/chainlink/AggregatorV3Interface.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import {IOracle} from "../../interfaces/IOracle.sol";
-import {Constants} from "../../../Constants.sol";
 import {Errors} from "../../../Errors.sol";
 
 /**
@@ -31,13 +27,7 @@ contract ChainlinkBasic is ChainlinkBase {
 
     function _getPriceOfToken(
         address token
-    )
-        internal
-        view
-        virtual
-        override(ChainlinkBase)
-        returns (uint256 tokenPriceRaw)
-    {
+    ) internal view virtual override returns (uint256 tokenPriceRaw) {
         tokenPriceRaw = token == BASE_CURRENCY
             ? BASE_CURRENCY_UNIT
             : super._getPriceOfToken(token);
