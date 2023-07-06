39948cf5e0eff64f87ea03694db5004b875ab2ef
asardon <as@myso.finance>
2023-07-07 01:41:06 +0200

    fixed test

--- contracts/test/UniV3TestSwap.sol
+++ contracts/test/UniV3TestSwap.sol
@@ -9,38 +9,34 @@ import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRoute
 
 contract UniV3TestSwap {
     ISwapRouter public immutable swapRouter;
-
-    // This example swaps DSETJ/WETH9 for single path swap
-    address public constant DSETH = 0x341c05c0E9b33C0E38d64de76516b2Ce970bB3BE;
-    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
-    // For this example, we will set the pool fee to 0.3%.
-    uint24 public constant poolFee = 100;
+    uint24 public constant poolFee = 3000;
 
     constructor(ISwapRouter _swapRouter) {
         swapRouter = _swapRouter;
     }
 
     function swapExactInputSingle(
+        address tokenIn,
+        address tokenOut,
         uint256 amountIn
     ) external returns (uint256 amountOut) {
         // msg.sender must approve this contract
 
-        // Transfer the specified amount of DAI to this contract.
+        // Transfer the specified amount to this contract.
         TransferHelper.safeTransferFrom(
-            WETH9,
+            tokenIn,
             msg.sender,
             address(this),
             amountIn
         );
 
-        // Approve the router to spend DAI.
-        TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        // Approve the router
+        TransferHelper.safeApprove(tokenIn, address(swapRouter), amountIn);
 
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
-                tokenIn: WETH9,
-                tokenOut: DSETH,
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
                 fee: poolFee,
                 recipient: msg.sender,
                 deadline: block.timestamp,
