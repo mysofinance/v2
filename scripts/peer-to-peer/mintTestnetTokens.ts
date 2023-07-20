4ecbd8fedeca732db4c976bc7c2c68b69d3f08d3
asardon <as@myso.finance>
2023-07-20 13:16:58 +0200

    added mint testnet tokens

---
+++ scripts/peer-to-peer/mintTestnetTokens.ts
@@ -0,0 +1,81 @@
+import { ethers } from 'hardhat'
+import * as readline from 'readline/promises'
+import { Logger, loadConfig } from '../helpers/misc'
+
+const hre = require('hardhat')
+const path = require('path')
+const scriptName = path.parse(__filename).name
+const logger = new Logger(__dirname, scriptName)
+
+async function main() {
+  logger.log(`Starting ${path.basename(__filename)}...`)
+  logger.log('Loading signer info (check hardhat.config.ts)...')
+
+  const [signer] = await ethers.getSigners()
+  const signerBal = await ethers.provider.getBalance(signer.address)
+  const network = await ethers.getDefaultProvider().getNetwork()
+  const hardhatNetworkName = hre.network.name
+  const hardhatChainId = hre.network.config.chainId
+
+  logger.log('Running script with the following signer:', signer.address)
+  logger.log('Signer ETH balance:', ethers.utils.formatEther(signerBal.toString()))
+  logger.log(`Interacting with network '${hardhatNetworkName}' (default provider network name '${network.name}')`)
+  logger.log(`Configured chain id '${hardhatChainId}' (default provider config chain id '${network.chainId}')`)
+  const expectedConfigFile = `/configs/${scriptName}.json`
+  logger.log(`Loading config '${expectedConfigFile}' with the following data:`)
+  const jsonConfig = loadConfig(__dirname, expectedConfigFile)
+  logger.log(JSON.stringify(jsonConfig[hardhatNetworkName]))
+
+  if (hardhatNetworkName in jsonConfig) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+
+    try {
+      const answer = await rl.question('Do you want to continue the script? [y/n] ', {
+        signal: AbortSignal.timeout(15_000) // 10s timeout
+      })
+
+      switch (answer.toLowerCase()) {
+        case 'y':
+          await mintTestnetTokens(signer, hardhatNetworkName, jsonConfig)
+          logger.log('Script completed.')
+          break
+        case 'n':
+          logger.log('Ending script.')
+          break
+        default:
+          logger.log('Invalid input.')
+          logger.log('Ending script.')
+      }
+    } finally {
+      rl.close()
+    }
+  } else {
+    logger.log(`No config defined for '${hardhatNetworkName}'!`)
+  }
+}
+
+async function mintTestnetTokens(signer: any, hardhatNetworkName: string, jsonConfig: any) {
+  logger.log(`Minting testnet tokens...`)
+
+  for (let mintInfo of jsonConfig[hardhatNetworkName]['mintInfo']) {
+    logger.log(`Loading testnet token with address '${mintInfo['tokenAddr']}'...`)
+    const TestnetToken = await ethers.getContractFactory('TestnetToken')
+    const testnetToken = TestnetToken.attach(mintInfo['tokenAddr'])
+    const owner = await testnetToken.owner()
+    if (owner == signer.address) {
+      logger.log(`Minting '${mintInfo['amount']}' to ${mintInfo['to']}...`)
+      await testnetToken.ownerMint(mintInfo['to'], mintInfo['amount'])
+      logger.log(`Minted.`)
+    } else {
+      logger.log(`Testnet token owner is '${owner}' and doesn't match signer '${signer.address}'.`)
+    }
+  }
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
